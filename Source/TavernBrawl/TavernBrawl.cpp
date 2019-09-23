3c483dd24cfa47705600b9add2f11ba13c198757
Jonas Andersen <20195223+NotBjoggisAtAll@users.noreply.github.com>
2019-09-23 14:28:54 +0200

    Added a TwinStick template project
    -Also added Content/StartContent to .gitignore

---
+++ Source/TavernBrawl/TavernBrawl.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "TavernBrawl.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TavernBrawl, "TavernBrawl" );
+
+DEFINE_LOG_CATEGORY(LogTavernBrawl)
+ 
